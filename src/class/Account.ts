7c9c4d849ed8cac180c4a50555290f622210186d
Marius <marius@example.com>
2022-07-15 09:26:49 +0300

    owneruzd

--- src/class/Account.ts
+++ src/class/Account.ts
@@ -5,30 +5,36 @@ class Account {
   // public - default, access from anywere
   // private - access from whitnin class
   // protected - access from within and inherited classes
-  readonly id: number;
-  owner: string;
-  private _balance: number;
-  username?: string | undefined;
 
-  constructor(id: number, owner: string, balance: number) {
-    this.id = id;
-    this.owner = owner;
-    this._balance = balance;
-  }
+  username?: string | undefined;
+  // paraterer properties
+  constructor(public readonly id: number, public owner: string, private _balance: number) {}
 
   deposit(value: number): void {
     if (value < 0) throw new Error('Invalid amount');
     // record transaction
     this._balance += value;
   }
+
+  private calculateTax(tax: number): void {
+    console.log(this._balance * tax);
+  }
+
+  getBalace(): number {
+    return this._balance;
+  }
 }
 
 const acc = new Account(1, 'Serbentautas', 5);
 acc.username = 'Serbas';
 // acc.id = 5; // error
-// acc._balance += 1001;
+console.log('acc.getBalace() ===', acc.getBalace());
 const acc2 = new Account(2, 'James Bond', 500);
 acc2.deposit(500);
+// padaryti owner nepasiekiama is isores
+acc2.owner = 'MIke';
+
+// pasirasom metoda kad gautu ownerio varda
 
 console.log('acc ===', acc);
 console.log('acc2 ===', acc2);
