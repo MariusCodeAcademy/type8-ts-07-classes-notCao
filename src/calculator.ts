be6a585d1eb713e37f9a243703cab88b9d867729
Marius <marius@example.com>
2022-07-18 12:01:38 +0300

    taikom

---
+++ src/calculator.ts
@@ -0,0 +1,5 @@
+// nusitaikom
+const num1InputEl = document.getElementById('n1') as HTMLInputElement | null;
+const num2InputEl = document.getElementById('n2') as HTMLInputElement | null;
+const caclulateEl = document.getElementById('caclulate') as HTMLButtonElement | null;
+const resultEl = document.getElementById('result') as HTMLButtonElement | null;
