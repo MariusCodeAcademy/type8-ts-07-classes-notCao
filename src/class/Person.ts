924a5a68b5fe0ef55071da4aa51ac512fa4829c1
Marius <marius@example.com>
2022-07-18 09:49:44 +0300

    todayWalked walk

--- src/class/Person.ts
+++ src/class/Person.ts
@@ -1,7 +1,10 @@
 // sukurti klase Person
 class Person {
   // firstName, lastName,
-  constructor(public firstName: string, public lastName: string) {}
+  private _todayWalked: number;
+  constructor(public firstName: string, public lastName: string) {
+    this._todayWalked = 0;
+  }
 
   // getter
   get fullName(): string {
@@ -12,14 +15,24 @@ class Person {
   // walk
   // walk metodas paduodam sk kilomentrais ir atspausdina kad nuejom xx kilometru
   walk(val: number): void {
-    console.log(`${this.firstName} walked ${val} km today. Yeyyy!`);
+    this._todayWalked += val;
+    console.log(`${this.firstName} walked ${val} km just now. Yeyyy!`);
+  }
+
+  // getteris today waked
+  get todayWalked(): number {
+    console.log(`${this.firstName} walked ${this._todayWalked} km Today. Yeyyy!`);
+    return this._todayWalked;
   }
 }
 
 const p1 = new Person('Severijus', 'Klaida');
 const p2 = new Person('Sebentautas', 'Niekada');
-console.log('p1 ===', p1);
 console.log(p1.fullName);
 p1.walk(5);
+p1.walk(5);
+p1.walk(5);
 p2.walk(7);
+// cp1onsole.log('p1 ===', p1);
+p1.todayWalked;
 // p1.fullName = 'Severijus';
